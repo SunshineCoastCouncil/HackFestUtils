8ea1c5b5d72524fc5aff6aaa3f8559a669b387c0
Patrick Dwyer <pat@patdwyer.com.au>
2016-11-16 16:13:27 +1000

    Create hackfest-participant group to prevent things like rdp login

--- CreateAllHackFestUsersAndShares.bat
+++ CreateAllHackFestUsersAndShares.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
+NET LOCALGROUP hackfest-participant /ADD
 FOR /f "tokens=1-2 delims=," %%a IN (HackFestUserNameAndPasswordList.csv) DO (
       NET USER %%a %%b /ADD
+      NET LOCALGROUP hackfest-participant %%a /ADD
       MKDIR E:\%%a
       NET SHARE %%a=E:\%%a /GRANT:%%a,FULL
 )
--- CreateHackFestUserAndShare.bat
+++ CreateHackFestUserAndShare.bat
@@ -2,5 +2,6 @@
 SET username=%1
 SET password=%2
 NET USER %username% %password% /ADD
+NET LOCALGROUP hackfest-participant %username% /ADD
 MKDIR E:\%username%
 NET SHARE %username%=E:\%username% /GRANT:%username%,FULL
